3f7c8692f8fad894387bfcb133087130d838a622
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-02-04 15:27:19 +0200

    implemented skeletons for the page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,3 +1,18 @@
+import { Paper } from '@mantine/core';
+
 export default function Page() {
-    return <h1>Settings page</h1>
-  }
+  return (
+    <div
+      style={{
+        display: 'flex', 
+        justifyContent: 'center',
+        width: '100vw',
+        height: '90vh',
+        marginTop: '7vh',
+      }}
+    >
+      <Paper style={{ width: '90vw', maxWidth: '400px', height: '78vh', marginTop: '2vh', backgroundColor: '#0e0e0e', paddingTop: '4vh', marginInline: '10px' }}>
+      </Paper>
+    </div>
+  );
+}
