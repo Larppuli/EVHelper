4e99a171eefe65f99fe75df28981cc353d246afb
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-02-04 15:40:37 +0200

    moved file to folder

---
+++ components/Header.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
+
+const Header: React.FC = () => {
+  const pathname = usePathname();
+  const pathnames = {
+    '/new-charging': 'New Charging',
+    '/chargings': 'Chargings',
+    '/settings': 'Settings',
+  }
+
+  return (
+    <div
+      style={{
+        backgroundColor: "#0e0e0e", 
+        color: "#c7c7c7", 
+        height: "60px",
+        display: "flex", 
+        justifyContent: "center", 
+        alignItems: "center",
+        borderBottom: "1px solid rgb(92, 92, 92)",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        }}
+    >
+      <h3 style={{fontSize: '20px'}}>{pathnames[pathname as keyof typeof pathnames]}</h3>
+    </div>
+  );
+};
+
+export default Header;
