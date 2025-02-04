a988088aabcfc182ae9409458f3fe99ecb244c88
Lauri Talvitie <lauri.talvitie@hotmail.com>
2025-02-04 15:31:43 +0200

    added PWA support

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,23 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
+import withPWA from 'next-pwa';
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-export default withBundleAnalyzer({
-  reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-  },
-});
+export default withBundleAnalyzer(
+  withPWA({
+    reactStrictMode: false,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    experimental: {
+      appDir: true,
+    },
+    pwa: {
+      dest: 'public',
+      register: true,
+      skipWaiting: true,
+    },
+  })
+);
